9a49d447218ea9b95f4f73f54b63951c92e5a563
zihahahaha <446200823@qq.com>
2024-05-10 13:05:28 +0800

    reset some file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,6 @@
 import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
 import useIpc from './ipc'
 
 function createWindow(): void {
@@ -12,7 +11,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    // ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js')
     }
